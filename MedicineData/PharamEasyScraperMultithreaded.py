65659c0afaa1e4493451d25accfe8d78f5ffd28a
puneetkapoor1212 <42838625+puneetkapoor1212@users.noreply.github.com>
2020-11-26 13:51:32 +0530

    multi_thread_code_update
    multi_thread_Code minor update

--- MedicineData/PharamEasyScraperMultithreaded.py
+++ MedicineData/PharamEasyScraperMultithreaded.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from database_functions import  mongo_insert
+from database_functions import  mongo_insert_multi_thread
 import concurrent.futures
 import string
 
@@ -21,16 +21,17 @@ def get_medicine_record(medicine_url):
     medicine_response = requests.get(medicine_url)
     medicine_data_soup = BeautifulSoup(medicine_response.content,'html.parser')
     medicine_data_record ={}
+    medicine_data_record['Vendor_Name'] = 'pharmaEasy'
     try:
         medicine_data_record['medicine_name'] = medicine_data_soup.find("h1",{"class":"ooufh"}).text
     except:
-        medicine_data_record['medicine_name'] = medicine_data_soup.find("h1",{"class":"ooufh"})
+        medicine_data_record['medicine_name'] = str(medicine_data_soup.find("h1",{"class":"ooufh"}))
         medicine_data_record['error_found'] = []
         medicine_data_record['error_found'].append('medcine_name not in order')
     try:
         medicine_data_record['medicine_producer'] = medicine_data_soup.find("div",{"class":"_3JVGI"}).text
     except:
-        medicine_data_record['medicine_producer'] = medicine_data_soup.find("div",{"class":"_3JVGI"})
+        medicine_data_record['medicine_producer'] = str(medicine_data_soup.find("div",{"class":"_3JVGI"}))
         if 'error_found' in medicine_data_record.keys():
             medicine_data_record['error_found'].append('medcine_producer not in order')
         else:
@@ -40,7 +41,7 @@ def get_medicine_record(medicine_url):
     try:
         medicine_data_record['medicine_number_of_strips'] = int(medicine_data_soup.find("div",{"class":"_36aef"}).text.split(" ")[0])
     except:
-        medicine_data_record['medicine_number_of_strips'] = medicine_data_soup.find("div",{"class":"_36aef"})
+        medicine_data_record['medicine_number_of_strips'] = str(medicine_data_soup.find("div",{"class":"_36aef"}))
         if 'error_found' in medicine_data_record.keys():
             medicine_data_record['error_found'].append('medcine_strip not in order')
         else:
@@ -49,7 +50,7 @@ def get_medicine_record(medicine_url):
     try:
         medicine_data_record['medicine_price'] = medicine_data_soup.find("div",{"class":"_1_yM9"}).text
     except:
-        medicine_data_record['medicine_price'] = medicine_data_soup.find("div",{"class":"_1_yM9"})
+        medicine_data_record['medicine_price'] = str(medicine_data_soup.find("div",{"class":"_1_yM9"}))
         if 'error_found' in medicine_data_record.keys():
             medicine_data_record['error_found'].append('medcine_price not in order')
         else:
@@ -59,7 +60,7 @@ def get_medicine_record(medicine_url):
     try:
         medicine_data_record['medicine_vendor_price'] =  float(medicine_data_soup.find("div",{"class":"_3FUtb"}).text.split("₹")[1])
     except:
-        medicine_data_record['medicine_vendor_price'] =  medicine_data_soup.find("div",{"class":"_3FUtb"})
+        medicine_data_record['medicine_vendor_price'] =  str(medicine_data_soup.find("div",{"class":"_3FUtb"}))
         if 'error_found' in medicine_data_record.keys():
             medicine_data_record['error_found'].append('medcine_vendor_price not in order')
         else:
@@ -69,7 +70,7 @@ def get_medicine_record(medicine_url):
     try:
         medicine_data_record['medicine_dicount'] = float(medicine_data_soup.find("div",{"class":"_306Fp"}).text.split("%")[0])
     except:
-        medicine_data_record['medicine_dicount'] = medicine_data_soup.find("div",{"class":"_306Fp"})
+        medicine_data_record['medicine_dicount'] = str(medicine_data_soup.find("div",{"class":"_306Fp"}))
         if 'error_found' in medicine_data_record.keys():
             medicine_data_record['error_found'].append('medcine_discount not in order')
         else:
@@ -78,7 +79,7 @@ def get_medicine_record(medicine_url):
     try:
         medicine_data_record['medicine_composition'] = medicine_data_soup.find("div",{"class":"_3Phld"}).text
     except:
-        medicine_data_record['medicine_composition'] = medicine_data_soup.find("div",{"class":"_3Phld"})
+        medicine_data_record['medicine_composition'] = str(medicine_data_soup.find("div",{"class":"_3Phld"}))
         if 'error_found' in medicine_data_record.keys():
             medicine_data_record['error_found'].append('medcine_composition not in order')
         else:
@@ -88,7 +89,7 @@ def get_medicine_record(medicine_url):
     try:
         medicine_data_record['medicine_theuraptic_Classification'] = medicine_data_soup.find("td",{"class":"_3C_XR"}).text
     except:
-        medicine_data_record['medicine_theuraptic_Classification'] = medicine_data_soup.find("td",{"class":"_3C_XR"})
+        medicine_data_record['medicine_theuraptic_Classification'] = str(medicine_data_soup.find("td",{"class":"_3C_XR"}))
         if 'error_found' in medicine_data_record.keys():
             medicine_data_record['error_found'].append('medcine_composition not in order')
         else:
@@ -97,6 +98,9 @@ def get_medicine_record(medicine_url):
 
     medicine_data_record_copy = medicine_data_record.copy() #dictionary copy into list addition
     medicine_records.append(medicine_data_record_copy)  #copying the dict to global list
+
+
+
 
 website_url= "https://pharmeasy.in/online-medicine-order/browse"
 website_alphabet_url = website_url + "?alphabet="
@@ -115,4 +119,5 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
     for url in medicine_urls:
         futures.append(executor.submit(get_medicine_record, url))
 
+mongo_insert_multi_thread(medicine_records)
 print(len(medicne_records))
