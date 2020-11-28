86edbe9362255a50765b60f5c4a3d6c47856ef04
puneetkapoor1212 <puneetkapoor1212@gmail.com>
2020-11-29 01:28:25 +0530

    query and strucuture formatting done
    Mongodb query for fetching the data based on regex matching and then grouping and displaying the latest updated data.

--- ReactApplication/Backend/schema/medicine_record.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');  
-var Schema = mongoose.Schema;  
-var Medicine = new Schema({      
-    name: { type: String   },       
-    address: { type: String   },     
-    email: { type: String },       
-    contact: { type: String },       
-},{ versionKey: false }); 
