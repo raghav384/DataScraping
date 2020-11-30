bbd1742f6384d1d1e26d13985d5c0cff4a872e2f
raghav384 <bansalraghav384@gmai.com>
2020-11-30 17:17:44 +0530

    scraping changes

--- DataScraping/testconfigparser.py
+++ DataScraping/testconfigparser.py
@@ -1,5 +1,6 @@
 import configparser
 import os
+import json
 
 #bsolute path calculation
 path_current_directory = os.path.dirname(__file__)
@@ -11,9 +12,16 @@ config.read(path_config_file)
 
 
 #print(config.optiond[1])
-for name, value in config.items(all_vendors):
-       print(value)
-``
+#for section_name in config.sections():
+ ##   for name, value in config.items("all_vendors"):
+   #    print(value)
+
+print(json.loads(config.get("pharmeasy","key_dictionary")))
+vendor_list = json.loads(config.get("all_vendors","vendors_list"))
+print(vendor_list)
+
+vendor ="pharmeasy"
+print(int(config[vendor]["start_product_id"]))
 #for section_name in config.sections():
 #    print('Section:', section_name)
  #   print('Options:', config.options(section_name))
