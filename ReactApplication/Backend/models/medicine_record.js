4275f9b297dc1bfecea82ba79a969bce7b1e1b32
raghav384 <bansalraghav384@gmai.com>
2020-12-04 15:45:32 +0530

    DataScraping Repo Initial Commit 4 December

--- ReactApplication/Backend/models/medicine_record.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');  
-
-
-var Schema = mongoose.Schema;
-var medicine_record = new Schema({      
-    medicine_name: { type: String   }                
-}); 
-
-module.exports = mongoose.model('medicine_records',medicine_record);
